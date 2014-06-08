6717cc600252b1d3a1590725505d4f46a5823fbe
Colin Hilliard <colin.hilliard1@gmail.com>
2014-06-08 12:38:25 -0500

    lesson price

---
+++ lessonprice.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const float priceper30= 17.0f / 30.0f;
+
+int main()
+{
+	float minutes;
+	float lessonprice;
+	
+	cout << "How many minutes is the lesson?";
+	cin >> minutes;
+	lessonprice = priceper30 * minutes;
+	cout << "The price for the lesson will be " << lessonprice << " dollars" << ".";
+	
+	return 0;
+}
